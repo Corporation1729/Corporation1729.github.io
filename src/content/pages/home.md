280948889f51710b3abd91eb323363ac789709ae
mvr <matthewvonrocketstein@gmail-dot-com>
2016-04-18 07:36:44 +0000

    initial commit

---
+++ src/content/pages/home.md
@@ -0,0 +1,11 @@
+sort_order: 1
+save_as: index.html
+Title: About Us
+Date: 2016-4-8
+Category: Python
+Tags: tag1, tag2
+Slug: home
+Authors: c1729
+Summary: About Us
+
+Corporation 1729 is a group of veteran architects, software developers, and UI / UX professionals, so we know what it takes to make well-documented, well-tested systems with superior engineering that "just work".  Whether you're looking for consulting services, project-based deliverables, or just hourly contractors to pick up the slack on your pre-existing work we'd be happy to talk with you about your needs.  Let's work together!
---
+++ src/pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'C1729'
+SITENAME = u'Corporation 1729'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+THEME = "theme"
+# Blogroll
+LINKS = (
+    ('github', 'https://github.com/orgs/Corporation1729/'),
+    ('placeholder1', '#'),
+    ('placeholder2', '#'),
+    ('placeholder3', '#'),
+    )
+
+# Social widget
+SOCIAL = (
+    ('placeholder4', '#'),
+    ('placeholder5', '#'),
+    ('placeholder6', '#'),
+    ('placeholder7', '#'),
+    )
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
